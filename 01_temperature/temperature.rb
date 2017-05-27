b197fd6aacdcda44ab98fe1bf181ae283b86ea6d
Luca Storani <lucastorani@gmail.com>
2017-05-27 10:20:08 +0100

    File fixed for test

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,10 @@
 #write your code here
+def ftoc(temperature)
+  temperature - 32 #freezing temperature
+  ((temperature - 32) * 5) / 9 #boiling water, body/arbitrary temperature
+end
+
+def ctof(temperature)
+  temperature + 32 #freezing temperature
+  ((temperature * 9.0) / 5.0) + 32 #boiling water, body/arbitrary temperature
+end
