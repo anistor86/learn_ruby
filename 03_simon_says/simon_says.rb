1a067d7f9005d35e902c196232edbb1963878552
Luca Storani <lucastorani@gmail.com>
2017-05-27 10:21:43 +0100

    File fixed for test

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,36 @@
 #write your code here
+def echo(expression)
+  expression
+end
+
+def shout(expression)
+  expression.upcase
+end
+
+def repeat(expression,*arr)
+  if arr != []
+    expression = ((expression + ' ') * arr[0]).strip!
+  else
+    expression + ' ' + expression
+  end
+end
+
+def start_of_word(expression, count)
+  expression[0, count]
+end
+
+def first_word(expression)
+  expression.split(" ").first
+
+end
+
+def titleize(expression)
+  small_words = %w[on the and if over for]
+  expression.split(' ').map.with_index do |w, i|
+    unless (small_words.include? w) and (i > 0)
+      w.capitalize
+    else
+      w
+    end
+  end.join(' ')
+end
