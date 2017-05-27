13e04bb7b2c411b717b7828ff262f04687765245
Luca Storani <lucastorani@gmail.com>
2017-05-27 10:20:32 +0100

    File fixed for test

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,24 @@
 #write your code here
+def add(a, b)
+  a + b
+end
+
+def subtract(a, b)
+  a - b
+end
+
+def sum(array)
+array.reduce(0, :+)
+end
+
+def multiply(array)
+  array.inject(:*)
+end
+
+def power(a, b)
+  a**b
+end
+
+def factorial(number)
+  (1..number).reduce(:*) || 1
+end
