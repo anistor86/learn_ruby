02d8de490d0693c0300efdde07bfa8b1f152fe3b
Luca Storani <lucastorani@gmail.com>
2017-05-29 10:53:49 +0100

    File fixed to pass the test

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,21 @@
 class Book
 # write your code here
+  attr_reader :title
+
+  def title=(text)
+    @title = text
+
+    small_words = %w[on the and if over for in of a an]
+    array_title = text.split(' ').map.with_index do |w, i|
+      unless (small_words.include? w) and (i > 0)
+        w.capitalize
+      else
+        w
+      end
+    end
+@title = array_title.join(' ')
+
+  end
+
+
 end
