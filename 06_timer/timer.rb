dbbef6b9954cc18db66a8aa777cd433672c6d356
Luca Storani <lucastorani@gmail.com>
2017-05-29 11:52:46 +0100

    File fixed to pass test

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,19 @@
 class Timer
   #write your code here
+  attr_accessor :seconds
+
+  def initialize
+    @seconds = 0
+  end
+
+  def time_string
+    seconds = @seconds % 60
+    minutes = (@seconds / 60) % 60
+    hours = @seconds / (60**2)
+    "#{padded(hours)}:#{padded(minutes)}:#{padded(seconds)}"
+  end
+
+  def padded(num)
+    return "%02d" % num
+  end
 end
